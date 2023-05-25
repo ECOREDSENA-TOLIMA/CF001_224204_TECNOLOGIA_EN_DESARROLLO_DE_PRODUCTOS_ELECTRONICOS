743e09712a3629ddd5bac0000c9e0a87c3e92c11
Francisco Vasquez <fjvasquez8@misena.edu.co>
2023-05-24 23:57:38 -0500

    Avances CF001
    Avances CF001

--- src/config/global.js
+++ src/config/global.js
@@ -1,19 +1,11 @@
 export default {
   global: {
-    componenteFormativo: 'NOMBRE DEL COMPONENTE FORMATIVO',
-    descripcionCurso: 'BREVE DESCRIPCIÓN',
-    imagenBannerPrincipal: require('@/assets/curso/portada/banner-princiapal.svg'),
+    componenteFormativo:
+      'Cálculo y medición de magnitudes eléctricas y electrónicas',
+    descripcionCurso:
+      'Comprender las magnitudes eléctricas permite tener un acercamiento al mundo de la electrónica; tal como un electrón cruza a través de los conductores, encontrando resistencia y transportando la corriente eléctrica, alimentándose del voltaje, y permitiendo la energía para que el circuito eléctrico tenga la potencia de trabajo.',
+    imagenBannerPrincipal: require('@/assets/curso/portada/banner-principal.svg'),
     fondoBannerPrincipal: require('@/assets/curso/portada/fondo-banner-principal.png'),
-    imagenesDecorativasBanner: [
-      {
-        clases: ['banner-principal-decorativo-1', 'd-none', 'd-lg-block'],
-        imagen: require('@/assets/curso/portada/banner-principal-decorativo-1.svg'),
-      },
-      {
-        clases: ['banner-principal-decorativo-2'],
-        imagen: require('@/assets/curso/portada/banner-principal-decorativo-2.svg'),
-      },
-    ],
   },
   menuPrincipal: {
     menu: [
@@ -24,35 +16,39 @@ export default {
       },
       {
         nombreRuta: 'introduccion',
-        icono: 'fas fa-info-circle',
+        icono: 'fas fa-info',
         titulo: 'Introducción',
         desarrolloContenidos: true,
       },
       {
         nombreRuta: 'tema1',
+        icono: 'far fa-file-alt',
         numero: '1',
-        titulo: 'Titulo de primer nivel',
+        titulo:
+          'Mediciones de acuerdo con procedimientos técnicos y especificaciones del fabricante',
         desarrolloContenidos: true,
         subMenu: [
           {
+            icono: 'far fa-file-alt',
             numero: '1.1',
-            titulo: 'Titulo de segundo nivel',
+            titulo: 'Mediciones e instrumentos de medición',
             hash: 't_1_1',
+          },
+          {
+            icono: 'far fa-file-alt',
+            numero: '1.2',
+            titulo: 'Magnitudes eléctricas y electrónicas',
+            hash: 't_1_2',
           },
         ],
       },
-
       {
         nombreRuta: 'tema2',
+        icono: 'far fa-file-alt',
         numero: '2',
-        titulo: 'Titulo de primer nivel',
+        titulo: 'Protocolos de pruebas',
         desarrolloContenidos: true,
-      },
-      {
-        nombreRuta: 'tema3',
-        numero: '3',
-        titulo: 'Titulo de primer nivel',
-        desarrolloContenidos: true,
+        subMenu: [],
       },
     ],
     subMenu: [
@@ -97,32 +93,57 @@ export default {
   },
   complementario: [
     {
-      tema: '',
-      referencia: '',
-      tipo: '',
-      link: 'https://www.google.com/',
+      texto:
+        'Hernández, N., Medel, P. y Ramírez, J. (2003). Dispositivos y circuitos electrónicos. Prácticas de laboratorio. Universidad Nacional Autónoma de México.',
+      tipo: 'PDF',
+      descarga: 'downloads/M_Dispositivos_Circuitos_Electronicos.pdf',
     },
     {
-      tema: '',
-      referencia: '',
-      tipo: '',
-      descarga: '/downloads/prueba.pdf',
+      texto: 'Arboledas, D. (2014). Electricidad básica. RA-MA.',
+      tipo: 'Libro',
+      link:
+        'https://elibro-net.bdigital.sena.edu.co/es/ereader/senavirtual/106570?page=24.',
     },
   ],
   glosario: [
     {
-      termino: '',
-      significado: '',
+      termino: 'Amperio',
+      significado: 'Unidad de corriente.',
     },
     {
-      termino: '<em></em>',
-      significado: '',
+      termino: 'Ángstrom',
+      significado:
+        'El ángstrom no es una unidad del sistema internacional de medidas. Sin embargo, está considerada como una de las unidades útiles para responder a necesidades específicas de ciertos campos científicos técnicos. Es la unidad de longitud equivalente a la diez mil millonésima (10-10) parte del metro. (Símb. Å).',
     },
-  ],
-  referencias: [
     {
-      referencia: '',
-      link: '',
+      termino: 'Continuidad',
+      significado: 'Facilidad de paso de la corriente.',
+    },
+    {
+      termino: 'Diferencia de potencial',
+      significado:
+        'Caída de voltaje al paso de la corriente por un determinado resistor o resistencia.',
+    },
+    {
+      termino: 'Estática',
+      significado:
+        'Relacionada con la física en electrónica y mecánica. En electrónica, se refiere a la energía que se almacena en elementos, y en mecánica, a elementos en quietud o sin movimiento.',
+    },
+    {
+      termino: 'Impedancia',
+      significado: 'Resistencia al paso de la corriente.',
+    },
+    {
+      termino: 'Julios',
+      significado: 'Unidad de energía.',
+    },
+    {
+      termino: 'Ohmios',
+      significado: 'Unidad de resistencia.',
+    },
+    {
+      termino: 'Voltios',
+      significado: 'Unidad de voltaje.',
     },
   ],
   creditos: [
--- src/config/titulo.js
+++ src/config/titulo.js
@@ -1,1 +1,1 @@
-module.exports = 'NOMBRE DEL COMPONENTE FORMATIVO'
+module.exports = 'Cálculo y medición de magnitudes eléctricas y electrónicas'
